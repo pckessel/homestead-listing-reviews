9b72e9c6de3f7fcde4f48cba1ee9b85af3b8197e
Phil Kessel <pckessel@gmail.com>
2018-11-09 11:46:03 -0800

    updated bundle for deployment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// for running on local machine, uncomment these lines and comment out the Window Assignment
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
